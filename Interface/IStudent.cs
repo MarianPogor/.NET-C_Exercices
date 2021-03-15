e42288528edcb2ff2628814cc40473390577a4bc
MarianPogor <marianpogor@gmail.com>
2021-03-15 12:42:27 +0100

    first commit
    Impementing interfaces in C#-

---
+++ Interface/IStudent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface IStudent
+    {
+        void register(int year);
+    }
+}
---
+++ Interface/PartTimeStudent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class PartTimeStudent : PartTimeEmployee, IStudent
+    {
+
+        private int startYear;
+        public int Startyear
+        {
+            get { return startYear; }
+            set { startYear = value; }
+        }
+        public PartTimeStudent(string name, double hourlyWage, int hoursPerMonth, int startYear) : 
+            base(name, hourlyWage, hoursPerMonth)
+        {
+            register(startYear);
+
+        }  
+
+        public void register(int startYear)
+        {
+            this.startYear = startYear;
+        }
+    }
+}
