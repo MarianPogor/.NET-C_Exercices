63d9a0a01bb2391c62ab69be0c9c522e3b802dcd
MarianPogor <marianpogor@gmail.com>
2021-03-15 12:47:22 +0100

    first commit
    Implementation of interfacein C#.

---
+++ HouseBusTaxes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseBusTaxes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ITaxable busTax = new Bus(123,3500, 1000, 13);
+            ITaxable housTax = new House("Arhus", true, 13, 8210);
+
+            Console.WriteLine("{0:g}", busTax.TaxValue());
+            Console.WriteLine("{0:g}", housTax.TaxValue());
+
+        }
+    }
+}
